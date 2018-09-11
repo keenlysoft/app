9628d7d96de2faf1d1d8e3f3ab8a90c26dbfcd51
yangbrain <qiaopi520@qq.com>
2018-09-11 12:23:08 +0800

    增加web

--- models/keenly.php
+++ models/keenly.php
@@ -13,7 +13,7 @@ define('DIR', DIRECTORY_SEPARATOR);
 class keenly{
     
     
-   private static $rootdir =  __DIR__ . DIR . '..' . DIR;
+   private static $rootdir =  __DIR__ . DIR . '..' . DIR; //根目录
    
    
    private static $current_dir = __DIR__ . DIR; //当前目录
@@ -26,6 +26,8 @@ class keenly{
    
    public static function run(){
        file_put_contents(self::$rootdir . 'keenly.install', 'installed at ' . date('Y-m-d H:i:s'));
+       file_put_contents(self::$rootdir . 'keenly', "<?php require './vendor/keenlysoft/keenly/bin/keenly';");
+       self::CreateWeb();
        self::unsetFile('keenly.php');
    }
    
@@ -36,5 +38,27 @@ class keenly{
    }
    
    
+   private static function CreateWeb(){
+       $web = self::$rootdir.'web';
+       @mkdir($web,0755);
+       $data = <<<'index'
+<?php
+/**
+ * This file is part of keenly from.
+ * @author brain_yang<qiaopi520@qq.com>
+ * (c) brain_yang
+ * github: https://github.com/keenlysoft/
+ * @time 2017年1月27日
+ * For the full copyright and license information, please view the LICENSE
+ */
+// Autoload 自动载入
+require '../vendor/autoload.php';
+// 路由配置
+require '../config/routes.php';
+index;
+
+       file_put_contents($web.'/index.php', $data);
+       
+   }
    
 }
